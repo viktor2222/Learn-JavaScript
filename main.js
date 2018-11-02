b6b3cc9f954825d8aaeb0cf24960dc31f055372b
v_panchuk <v.panchuk@pozitiff.ua>
2018-11-02 12:43:10 +0200

    static-methods

--- main.js
+++ main.js
@@ -1449,3 +1449,49 @@ console.log(nurseOlynyk.remainingVacationDays);
 //At the bottom of main.js call the .addCertification() method on nurseOlynyk with a parameter of 'Genetics'.
 nurseOlynyk.addCertification('Genetics');
 console.log(nurseOlynyk.certifications);
+
+//Static Methods
+
+class HospitalEmployee {
+  constructor(name) {
+    this._name = name;
+    this._remainingVacationDays = 20;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
+  }
+  //Inside of your HospitalEmployee class, create a static method called generatePassword. When it's called, this method should return a random integer between zero and 10,000.
+  static generatePassword(){
+    return Math.floor(Math.random() * 10000);
+  }
+}
+
+class Nurse extends HospitalEmployee {
+  constructor(name, certifications) {
+    super(name);
+    this._certifications = certifications;
+  }
+
+  get certifications() {
+    return this._certifications;
+  }
+
+  addCertification(newCertification) {
+    this.certifications.push(newCertification);
+  }
+}
+
+const nurseOlynyk = new Nurse('Olynyk', ['Trauma','Pediatrics']);
+nurseOlynyk.takeVacationDays(5);
+console.log(nurseOlynyk.remainingVacationDays);
+nurseOlynyk.addCertification('Genetics');
+console.log(nurseOlynyk.certifications);
