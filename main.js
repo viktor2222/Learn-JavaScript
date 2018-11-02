03009736bffd8c7c10acb7fcae1b3dbac725d028
v_panchuk <v.panchuk@pozitiff.ua>
2018-11-02 11:52:50 +0200

    inheritance V

--- main.js
+++ main.js
@@ -1406,3 +1406,46 @@ const nurseOlynyk = new Nurse('Olynyk', ['Trauma','Pediatrics']);
 //Call .takeVacationDays() with an input of 5 on your nurseOlynyk instance.
 nurseOlynyk.takeVacationDays(5);
 console.log(nurseOlynyk);
+
+//Inheritance V
+
+class HospitalEmployee {
+  constructor(name) {
+    this._name = name;
+    this._remainingVacationDays = 20;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
+  }
+}
+
+class Nurse extends HospitalEmployee {
+  constructor(name, certifications) {
+    super(name);
+    this._certifications = certifications;
+  }
+//Use the properties and methods below to help you complete the tasks that follow. Nurse Properties: _name, _remainingVacationDays (set to 20 inside the constructor()), _certifications Methods: .takeVacationDays(), .addCertification() Under the Nurse constructor(), add a getter that returns the value saved to the Nurse instance's _certifications.
+  get certifications(){
+    return this._certifications;
+  }
+  addCertification(newCertification){
+    this._certifications.push(newCertification);
+  }
+}
+
+const nurseOlynyk = new Nurse('Olynyk', ['Trauma','Pediatrics']);
+nurseOlynyk.takeVacationDays(5);
+console.log(nurseOlynyk.remainingVacationDays);
+
+//At the bottom of main.js call the .addCertification() method on nurseOlynyk with a parameter of 'Genetics'.
+nurseOlynyk.addCertification('Genetics');
+console.log(nurseOlynyk.certifications);
