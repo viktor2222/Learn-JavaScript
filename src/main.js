868d2e0192a37e49a8a796d00f1622f33c009b87
Viktor Panchuk <v.panchuk@pozitiff.ua>
2018-11-02 22:24:43 +0200

    learn-babel

--- src/main.js
+++ src/main.js
@@ -1524,3 +1524,5 @@ var sauce = "Eggs and cheese"; // ES6 syntax
 
 // Template literals, like the one below, were introduced in ES6
 var carbonara = "You can make carbonara with" + pasta + meat + "and a sauce made with" + sauce;
+
+//
