872ddbc4ad48be8287575a06c6e9c41fb354eff8
v_panchuk <v.panchuk@pozitiff.ua>
2018-11-05 19:04:38 +0200

    require

--- src/main.js
+++ src/main.js
@@ -1536,3 +1536,15 @@ Airplane.myAirplane = "StarJet";
 
 //Export the module.
 module.exports = Airplane;
+
+// require()
+
+//In 1-missionControl.js use the require() function to import the Airplane module from 1-airplane.js. Recall that you will need to use the precise name of the file that contains the module.
+const Airplane = require('./1-airplane.js');
+
+//In 1-missionControl.js, define a function displayAirplane(). In the function, log the value of the module and its property to the console.
+function displayAirplane(){
+  console.log(Airplane.myAirplane);
+}
+
+displayAirplane();
