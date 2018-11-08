7fafc2cee61ec32a5031d28142b634ce68eaf14f
v_panchuk <v.panchuk@pozitiff.ua>
2018-11-08 11:24:17 +0200

    XHR POST Requests II

--- src/main.js
+++ src/main.js
@@ -1985,3 +1985,30 @@ const displaySuggestions = (event) => {
 }
 
 submit.addEventListener('click', displaySuggestions);
+
+//XHR POST Requests II
+
+//Create a new XMLHttpRequest object using the new operator, and save it in a const called xhr.
+const xhr = new XMLHttpRequest();
+
+//Next, save the following URL to a const called url. Make sure the URL is wrapped in quotes so that it is a string.
+const url = 'https://api-to-call.com/endpoint';
+
+//Create a new const called data, and save this line of code to it: JSON.stringify({id: '200'}); JSON.stringify() will convert a value to a JSON string. By converting the value to a string, we can then send the data to a server.
+const data = JSON.stringify({id: '200'});
+
+//Set the responseType property of the xhr object to be 'json'.
+xhr.responseType = 'json';
+
+//Set the xhr.onreadystatechange event handler equal to an anonymous arrow function. Leave the function empty until the next step. .onreadystatechange will contain the event handler that will be called when xhr's state changes.In the code block of the function you created in the previous step, add a conditional statement that checks to see if the readyState of xhr is equal to XMLHttpRequest.DONE.
+xhr.onreadystatechange = () => {
+  if(xhr.readyState === XMLHttpRequest.DONE){
+     return xhr.response;
+  }
+}
+
+//Below the function you created in the previous two steps, call the .open() method on the xhr object and pass it 'POST' and url as arguments. .open() creates a new request and the arguments passed in determine what type of request is being made and where it's being made to.
+xhr.open('POST', url);
+
+//On the following line, call the .send() method on the xhr object and pass data as an argument. .send() will send the request to the server. Nice work! You've written the boilerplate code for an AJAX POST request using an XMLHttpRequest object.
+xhr.send(data);
