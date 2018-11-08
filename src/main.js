27113f104df2f8f1455cbe4fa44612ef8c74d538
v_panchuk <v.panchuk@pozitiff.ua>
2018-11-08 18:09:32 +0200

    fetch() GET

--- src/main.js
+++ src/main.js
@@ -2057,3 +2057,20 @@ const displayShortUrl = (event) => {
 shortenButton.addEventListener('click', displayShortUrl);
 
 //Enter this URL into the input field, and click the shorten button in the web page. https://medium.com/@codecademy/breaking-the-coding-language-barrier-bf24652c3c60 Notice the object that came back! Now replace renderRawResponse(xhr.response) with renderResponse(xhr.response). Run the code. Paste the URL again and click "Shorten". Isn't it much cleaner?
+
+
+//REQUESTS II
+
+//fetch() GET Requests II
+
+//First, call the fetch() function and pass it this URL as a string: https://api-to-call.com/endpoint This first argument determines the endpoint of the request. Chain a .then() method to the end of the fetch() function and pass it the success callback arrow function as its first argument. The success callback function takes one parameter, response. .then() will fire only after the promise status of fetch() has been resolved.Inside of the response callback function, check the ok property of response inside of a conditional statement. In the code block of the conditional statement, return response.json(). The reason we're testing the ok property of the response object is that it will be a Boolean value. If there were no errors response.ok will be true and then your code will return response.json(). Below the curly braces of the conditional statement, create a new error with this code: throw new Error('Request failed!');Add a second argument to .then(), it will be an arrow function that will handle our failures. Separate the first callback function from the second with a comma. The second callback function takes a single parameter, networkError. In the code block of the second callback function, log networkError.message to the console. If we could not reach the endpoint at all, e.g., the server is down, then we would get this networkError.
+fetch('https://api-to-call.com/endpoint').then(response =>{
+  if(response.ok){
+    return response.json();
+  }
+  throw new Error('Request failed!');
+}, networkError =>{
+  console.log(networkError.message);
+}).then(jsonResponse =>{
+  return jsonResponse;
+});
