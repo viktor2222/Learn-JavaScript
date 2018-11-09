e73dc3b767aacd8303478a19885d6f0387995dc5
v_panchuk <v.panchuk@pozitiff.ua>
2018-11-09 18:15:52 +0200

    async POST Requests III

--- src/main.js
+++ src/main.js
@@ -2378,3 +2378,45 @@ const getData = async () => {
     console.log(error);//In the code block for getData, we should add a try statement with an empty code block. In case things go wrong, we need to some code to handle that. Below the try code block, add a catch statement and pass in error as an argument. Then, in the catch statement code block, log error to the console.
   }
 };
+
+//async POST Requests III
+
+// information to reach API At the top of main.js, assign apiKey to your Rebrandly API key.
+const apiKey = 'd16eb188835242829c7ed28be9a1c790';
+const url = 'https://api.rebrandly.com/v1/links';
+
+// Some page elements
+const inputField = document.querySelector('#input');
+const shortenButton = document.querySelector('#shorten');
+const responseField = document.querySelector('#responseField');
+
+// AJAX functions Under the comment "AJAX functions", create a new arrow function and assign it to a const shortenUrl() using the async keyword. Inside the code block of the arrow function of shortenUrl create two consts: One named urlToShorten and assign it inputField.value. The other named data and assign it the value of calling JSON.stringify() and passing in {destination: urlToShorten}. Please note, we will be working inside shortenUrl() for the remainder of the exercise.
+const shortenUrl = async () => {
+  const urlToShorten = inputField.value;
+  const data = JSON.stringify({destination: urlToShorten});
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      body: data,
+      headers: { 'Content-type': 'application/json', 'apikey': apiKey }
+    });//Inside the try code block, using const, create a variable named response and assign it to await the value of calling calling fetch().
+  	if(response.ok){//Below the variable response from the previous step, create a conditional statement that the checks if the ok property of response evaluates to a truthy value.
+    	const jsonResponse = await response.json();
+      renderResponse(jsonResponse);//Call the function renderRawResponse() and pass in jsonResponse. Then, run the code. Now it’s time to clean up the response sent back. Delete renderRawResponse(jsonResponse) and replace it with renderResponse(jsonResponse). Run the code. Then paste in the URL again and click the shorten button.
+    }
+  } catch(error) {
+    console.log(error);//Add a try statement and leave the code block empty for now. After the try code block, create a catch statement and pass in error. In the code block of catch(error), log error to the console.
+  }
+}
+// Code goes here
+
+// Clear page and call AJAX functions
+const displayShortUrl = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild);
+  }
+  shortenUrl();
+}
+
+shortenButton.addEventListener('click', displayShortUrl);
