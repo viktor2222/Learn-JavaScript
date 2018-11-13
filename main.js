3641026f9bab05845d98322048beefce0868667b
v_panchuk <v.panchuk@pozitiff.ua>
2018-11-13 18:09:30 +0200

    Show the sign

--- main.js
+++ main.js
@@ -8,10 +8,18 @@
 // let name = prompt('What is your name', '');
 //
 // alert('Your name: ' + name);
-let officialNameScript = prompt('What is the “official” name of JavaScript?','');
-
-if(officialNameScript == 'ECMAScript'){
-  alert('Right!');
-} else{
-  alert('Didn’t know? ECMAScript!');
+// let officialNameScript = prompt('What is the “official” name of JavaScript?','');
+//
+// if(officialNameScript == 'ECMAScript'){
+//   alert('Right!');
+// } else{
+//   alert('Didn’t know? ECMAScript!');
+// }
+let number = prompt('Please enter number','');
+if(number > 0){
+  alert(1);
+} else if (number < 0) {
+  alert(-1);
+} else {
+  alert(0);
 }
