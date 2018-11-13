4049b7b4d391d216496b9c693cc1368f916b043b
v_panchuk <v.panchuk@pozitiff.ua>
2018-11-13 18:44:18 +0200

    rewriteif

--- main.js
+++ main.js
@@ -40,3 +40,10 @@ if(userName == 'Admin'){
 } else {
   alert('I don’t know you');
 }
+
+if (a + b < 4) {
+  result = 'Below';
+} else {
+  result = 'Over';
+}
+result = (a + b < 4) ? 'Below' : 'Over';
