67de0e767d1e50876f7e2cb78824a2917a109d3b
v_panchuk <v.panchuk@pozitiff.ua>
2018-11-13 17:22:07 +0200

    simplePage

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-alert('Я – JavaScript!');
+// alert('Я – JavaScript!');
+//
+// let admin;
+// let name = "John";
+// admin = name;
+// alert(admin);
+
+let name = prompt('What is your name', '');
+
+alert('Your name: ' + name);
