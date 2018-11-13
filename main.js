1b521e6a96ffcd0b0d56601b61f387f85e45682e
v_panchuk <v.panchuk@pozitiff.ua>
2018-11-13 18:53:31 +0200

    rewriteIfElse

--- main.js
+++ main.js
@@ -47,3 +47,20 @@ if (a + b < 4) {
   result = 'Over';
 }
 result = (a + b < 4) ? 'Below' : 'Over';
+
+let message;
+
+if (login == 'Employee') {
+  message = 'Hello';
+} else if (login == 'Director') {
+  message = 'Greetings';
+} else if (login == '') {
+  message = 'No login';
+} else {
+  message = '';
+}
+
+message = (login == 'Employee') ? 'Hello' :
+  (login == 'Director') ? 'Greetings' :
+  (login == '') ? 'No login' :
+  '';
