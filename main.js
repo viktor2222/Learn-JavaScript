e8e9dd41f22aff70002fd504e710d8f576332b0c
v_panchuk <v.panchuk@pozitiff.ua>
2018-11-14 15:36:15 +0200

    rewriteIf

---
+++ main.js
@@ -0,0 +1,17 @@
+//Rewrite "if" into "switch"
+var a = +prompt('a?', '');
+
+switch (a) {
+  case 0:
+    alert(0);
+    break;
+  case 1:
+    alert(1);
+    break;
+  case 2:
+  case 3:
+    alert('2,3');
+    break;
+  default:
+    
+}
