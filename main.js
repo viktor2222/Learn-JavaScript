bb14582d6858fae91ee80b91db97b34fab7935d8
v_panchuk <v.panchuk@pozitiff.ua>
2018-11-14 13:30:42 +0200

    loops

--- main.js
+++ main.js
@@ -1,74 +1,15 @@
-// alert('Я – JavaScript!');
-//
-// let admin;
-// let name = "John";
-// admin = name;
-// alert(admin);
+//Output even numbers in the loop
 
-// let name = prompt('What is your name', '');
-//
-// alert('Your name: ' + name);
-// let officialNameScript = prompt('What is the “official” name of JavaScript?','');
-//
-// if(officialNameScript == 'ECMAScript'){
-//   alert('Right!');
-// } else{
-//   alert('Didn’t know? ECMAScript!');
-// }
-// let number = prompt('Please enter number','');
-// if(number > 0){
-//   alert(1);
-// } else if (number < 0) {
-//   alert(-1);
-// } else {
-//   alert(0);
-// }
-// let userName = prompt('Please enter your login');
-//
-// if(userName == 'Admin'){
-//   let password = prompt('enter your password', '');
-//
-//   if(password == 'TheMaster'){
-//     alert('Welcome!');
-//   } else if (password == null) {
-//     alert('Canceled.');
-//   } else {
-//     alert('Wrong password');
-//   }
-// } else if (userName == null){
-//   alert('Canceled.');
-// } else {
-//   alert('I don’t know you');
-// }
-//
-// if (a + b < 4) {
-//   result = 'Below';
-// } else {
-//   result = 'Over';
-// }
-// result = (a + b < 4) ? 'Below' : 'Over';
-//
-// let message;
-//
-// if (login == 'Employee') {
-//   message = 'Hello';
-// } else if (login == 'Director') {
-//   message = 'Greetings';
-// } else if (login == '') {
-//   message = 'No login';
-// } else {
-//   message = '';
-// }
-//
-// message = (login == 'Employee') ? 'Hello' :
-//   (login == 'Director') ? 'Greetings' :
-//   (login == '') ? 'No login' :
-//   '';
+for(let evenNumber = 2; evenNumber <= 10; evenNumber++){
+  if(evenNumber % 2 == 0){
+    alert(evenNumber);
+  }
+}
 
-//Check the range between
-let age;
-if(age >= 14 && age <= 90) {}
+//Repeat until the input is correct
+let greaterNumber;
+do{
+  greaterNumber = prompt('Enter number greater than 100', 0)
+} while (greaterNumber <= 100 && greaterNumber != null) {
 
-//Check the range outside
-if(!(age >=14 && age <=90)){}
-if(age <=14 && age >=90){}
+}
