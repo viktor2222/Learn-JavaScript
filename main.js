7d9924fa24e23c28ae8efb1966b48af1f39b418e
v_panchuk <v.panchuk@pozitiff.ua>
2018-11-14 15:16:28 +0200

    clear

--- main.js
+++ main.js
@@ -1,8 +1,1 @@
-//Output prime numbers
-nextTime:
-for(let primeNumber = 2; primeNumber <= 10; primeNumber++){
-  for(a = 2; a < primeNumber; a++){
-    if(primeNumber % a == 0) continue nextTime;
-  }
-  alert(primeNumber);
-}
+
