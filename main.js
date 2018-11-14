7e7eeb9441b35dcce20fd52b52f84126acb047f6
v_panchuk <v.panchuk@pozitiff.ua>
2018-11-14 18:04:03 +0200

    clear

--- main.js
+++ main.js
@@ -14,27 +14,27 @@
 //   return (age > 18) || confirm('Родители разрешили?');
 // }
 //Function min(a, b)
-function min(a, b) {
-  if(a < b){
-    return a;
-  } else {
-    return b;
-  }
-}
-//Функция pow(x,n)
-function pow(x, n){
-  let result = x;
-
-  for(a = 1; a < n; a++){
-    result *= x;
-  }
-  return result;
-}
-let x = prompt('enter x');
-let n = prompt('enter n');
-
-if(n <= 1){
-  alert('Power ' + n + ' not supported, please enter good power and > 1');
-} else{
-  alert(pow(x, n));
-}
+// function min(a, b) {
+//   if(a < b){
+//     return a;
+//   } else {
+//     return b;
+//   }
+// }
+// //Функция pow(x,n)
+// function pow(x, n){
+//   let result = x;
+//
+//   for(a = 1; a < n; a++){
+//     result *= x;
+//   }
+//   return result;
+// }
+// let x = prompt('enter x');
+// let n = prompt('enter n');
+//
+// if(n <= 1){
+//   alert('Power ' + n + ' not supported, please enter good power and > 1');
+// } else{
+//   alert(pow(x, n));
+// }
