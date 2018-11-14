7caa00630cc6fc8eccce103eca7722205153b107
v_panchuk <v.panchuk@pozitiff.ua>
2018-11-14 17:58:13 +0200

    function

--- main.js
+++ main.js
@@ -1,15 +1,40 @@
 //Rewrite the function using '?' or '||'
-function checkAge(age) {
-  if (age > 18) {
-    return true;
+// function checkAge(age) {
+//   if (age > 18) {
+//     return true;
+//   } else {
+//     return confirm('Родители разрешили?');
+//   }
+// }
+// function checkAge(age){
+//   return (age > 18) ? true : confirm('Родители разрешили?');
+// }
+//
+// function checkAge(age){
+//   return (age > 18) || confirm('Родители разрешили?');
+// }
+//Function min(a, b)
+function min(a, b) {
+  if(a < b){
+    return a;
   } else {
-    return confirm('Родители разрешили?');
+    return b;
   }
 }
-function checkAge(age){
-  return (age > 18) ? true : confirm('Родители разрешили?');
-}
+//Функция pow(x,n)
+function pow(x, n){
+  let result = x;
 
-function checkAge(age){
-  return (age > 18) || confirm('Родители разрешили?');
+  for(a = 1; a < n; a++){
+    result *= x;
+  }
+  return result;
+}
+let x = prompt('enter x');
+let n = prompt('enter n');
+
+if(n <= 1){
+  alert('Power ' + n + ' not supported, please enter good power and > 1');
+} else{
+  alert(pow(x, n));
 }
