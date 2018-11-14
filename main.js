5d0930fa7ec18a26d4c9264cf61a7752965f02ec
v_panchuk <v.panchuk@pozitiff.ua>
2018-11-14 17:31:51 +0200

    rewriteFunction

---
+++ main.js
@@ -0,0 +1,15 @@
+//Rewrite the function using '?' or '||'
+function checkAge(age) {
+  if (age > 18) {
+    return true;
+  } else {
+    return confirm('Родители разрешили?');
+  }
+}
+function checkAge(age){
+  return (age > 18) ? true : confirm('Родители разрешили?');
+}
+
+function checkAge(age){
+  return (age > 18) || confirm('Родители разрешили?');
+}
