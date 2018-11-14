92f47b188c0adc182fbeca2ee5fbb53602385cf1
v_panchuk <v.panchuk@pozitiff.ua>
2018-11-14 14:03:55 +0200

    clear

--- main.js
+++ main.js
@@ -1,15 +1,1 @@
-//Output even numbers in the loop
 
-for(let evenNumber = 2; evenNumber <= 10; evenNumber++){
-  if(evenNumber % 2 == 0){
-    alert(evenNumber);
-  }
-}
-
-//Repeat until the input is correct
-let greaterNumber;
-do{
-  greaterNumber = prompt('Enter number greater than 100', 0)
-} while (greaterNumber <= 100 && greaterNumber != null) {
-
-}
